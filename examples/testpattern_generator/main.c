0ff28959fb4417788a1281efe4a098f1a17574b0
Julian Dievernich <julian.dievernich@solectrix.de>
2023-09-25 12:17:46 +0200

    bump testpattern generator

--- examples/testpattern_generator/main.c
+++ examples/testpattern_generator/main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-#include <pthread.h>
-#include <errno.h>
 #include <unistd.h>
-#include <termios.h>
-#include <sys/mman.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <linux/videodev2.h>
+#include <stdlib.h>
+
+/** Features
+ * Add GVSP testpattern
+ * Add incremental testpattern
+ * Add sawtooth testpattern
+ * Add bar testpattern
+ * Add output filename
+ */
 
 static int testpattern_store(size_t count) {
     char fn[256];
     int fd, ret;
     size_t size = 0;
+    uint8_t val;
 
     if(snprintf(fn, sizeof(fn), "testpattern.bin") < 0)
         goto err;
@@ -23,7 +30,8 @@ static int testpattern_store(size_t count) {
         goto err;
 
     while(size < count) {
-        ret = write(fd, (uint8_t)(size & 0xff), 1);
+        val = (uint8_t)(size & 0xff);
+        ret = write(fd, &val, 1);
         if(ret < 0)
             goto err_close;
 
@@ -45,12 +53,10 @@ static void usage(const char *argv0) {
 }
 
 int main(int argc, char **argv) {
-    int ret;
-
     if(argc < 1) {
     usage(argv[0]);
         return -1;
     }
 
-    return testpattern_store(atol(argv[1]));
+    return testpattern_store(strtoul(argv[1], NULL, 0));
 }
