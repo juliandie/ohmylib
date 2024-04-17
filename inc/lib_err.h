048ee6b555b0645639c7def46b7b78cc6f4e6ac5
Julian Dievernich <julian.dievernich@solectrix.de>
2024-04-18 01:13:31 +0200

    fix type issues

--- inc/lib_err.h
+++ inc/lib_err.h
@@ -1,33 +1,21 @@
 #ifndef LIB_ERR_H_
 #define LIB_ERR_H_
 
-// see <linux/err.h>
+#include <stdbool.h>
+/* see <linux/err.h> for reference */
 
-#define MAX_ERRNO	(4095)
+#define IS_ERR_VALUE(x) ((unsigned long)(x) >= (unsigned long)-(0xfff))
 
-#define IS_ERR_VALUE(x) ((unsigned long)(void *)(x) >= (unsigned long)-MAX_ERRNO)
+static inline void* ERR_PTR(long err) \
+{ return (void*)err; }
 
-static inline void * ERR_PTR(long error) {
-	return (void *) error;
-}
+static inline long PTR_ERR(const void* p) \
+{ return (long)p; }
 
-static inline long PTR_ERR(const void *ptr) {
-	return (long) ptr;
-}
+static inline bool IS_ERR(const void* p) \
+{ return IS_ERR_VALUE(p); }
 
-static inline bool IS_ERR(const void *ptr) {
-	return IS_ERR_VALUE((unsigned long)ptr);
-}
+static inline int IS_ERR_OR_NULL(const void* p) \
+{ return ((!p) || IS_ERR_VALUE(p)); }
 
-static inline bool IS_ERR_OR_NULL(const void *ptr) {
-	return (ptr == NULL) || IS_ERR_VALUE((unsigned long)ptr);
-}
-
-static inline int PTR_ERR_OR_ZERO(const void *ptr) {
-	if (IS_ERR(ptr))
-		return PTR_ERR(ptr);
-	else
-		return 0;
-}
-
-#endif /* LIB_ERR_H_ */
+#endif
